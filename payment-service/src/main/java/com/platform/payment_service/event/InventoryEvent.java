4a4fadb3ac5c0e995ff9f14b013ea0f7614b0d82
Jaswanthi Golla <jaswanthi.golla@gmail.com>
2026-03-16 15:53:39 +0530

    feat: add InventoryEvent model for consuming inventory.reserved events

---
+++ payment-service/src/main/java/com/platform/payment_service/event/InventoryEvent.java
@@ -0,0 +1,23 @@
+package com.platform.paymentservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryEvent {
+    private String eventId;
+    private String orderId;
+    private String customerId;
+    private String productId;
+    private Integer quantity;
+    private String status;
+    private String reason;
+    private LocalDateTime timestamp;
+}
