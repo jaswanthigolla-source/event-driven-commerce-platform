83e9f898d180a8147ffc1439b080809f28b9517d
Jaswanthi Golla <jaswanthi.golla@gmail.com>
2026-03-16 15:51:12 +0530

    feat: add OrderEvent model for consuming order.created events

---
+++ inventory-service/src/main/java/com/platform/inventory_service/event/OrderEvent.java
@@ -0,0 +1,24 @@
+package com.platform.inventoryservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEvent {
+    private String eventId;
+    private String orderId;
+    private String customerId;
+    private String productId;
+    private Integer quantity;
+    private BigDecimal totalAmount;
+    private String status;
+    private LocalDateTime timestamp;
+}
